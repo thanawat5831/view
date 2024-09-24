c3c8b8705a7987924b0e24c87e0f104a9a6b62aa
ROIHEE <thanawat58311@gmail.com>
2024-09-24 15:02:48 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+        'url'		=> 'https://www.np.ac.th/np/newdemo/web/',
+        'parameter' => 'go',
+        'template'  => 'temp.html',
+        'keywords'  => 'bb.txt',
+        'sitemap'	=> 'gg.xml'
+);
